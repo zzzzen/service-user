3f28cd1a2d1da166994329e7a250f8592603bb91
da.zakovriashin <da.zakovriashin@severstal.com>
2024-01-30 15:11:01 +0200

    1

---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGO_CONNECTION_STRING),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
